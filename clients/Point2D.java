b902f650b278b60db7a80df521a61b0db2f89591
m0sys <21181457+m0sys@users.noreply.github.com>
2022-09-12 15:10:18 -0400

    refactor: change directory struct to match p75

---
+++ clients/Point2D.java
@@ -0,0 +1,7 @@
+/*
+ * Sedgewick 4th Edition: S1.2 - Data Abstraction
+ *
+ * Problem 1.2.1: Write a Point2D client that takes an integer value N from the
+ * command line, generates N random points in the unit square, and computes the
+ * distance separating the closest pair of points.
+ */
