a506a33c380c163015a1d97ff323478b272fd88a
m0sys <21181457+m0sys@users.noreply.github.com>
2022-09-14 19:59:09 -0400

    feat(s1): add some throw smartdate impl

---
+++ src/main/java/m0/algo/other/Date.java
@@ -0,0 +1,28 @@
+package com.m0.algo.other;
+
+/*
+ * Sedgewick 4th Edition: S1.2 - Data Abstraction
+ *
+ * Problem 1.2.11: Develop an implementation SmartDate of our Date API that
+ * raises an exception if the date is not legal.
+ */
+
+public class Date {
+  private final int month;
+  private final int day;
+  private final int year;
+
+  public Date(int m, int d, int y) {
+    month = m;
+    day = d;
+    year = y;
+  }
+
+  public int month() { return month; }
+
+  public int day() { return day; }
+
+  public int year() { return year; }
+
+  public String toString() { return month() + "/" + day() + "/" + year(); }
+}
---
+++ src/main/java/m0/algo/other/SmartDate.java
@@ -0,0 +1,24 @@
+package com.m0.algo.other;
+
+/*
+ * Sedgewick 4th Edition: S1.2 - Data Abstraction
+ *
+ * Problem 1.2.11: Develop an implementation SmartDate of our Date API that
+ * raises an exception if the date is not legal.
+ */
+
+import com.m0.algo.other.Date;
+
+public class SmartDate extends Date {
+
+  public SmartDate(int m, int d, int y) {
+    super(m, d, y);
+    if (m < 0 || m > 12)
+      throw new IllegalArgumentException(
+          "error: month has to be between 1 and 12");
+
+    if (d < 0 || d > 31)
+      throw new IllegalArgumentException(
+          "error: year has to be between 1 and 31");
+  }
+}
