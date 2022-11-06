cfecf4df5bc1e332dd10e9235407f9272acec4c0
m0sys <21181457+m0sys@users.noreply.github.com>
2022-11-06 18:39:58 -0500

    test: add tests for max pq

--- src/test/java/com/m0/algo/collections/MaxPQTest.java
+++ src/test/java/com/m0/algo/collections/MaxPQTest.java
@@ -14,4 +14,29 @@ public class MaxPQTest {
     pq.insert(20);
     assertFalse(pq.isEmpty());
   }
+
+  @Test
+  public void testSize() {
+    MaxPQ pq = new MaxPQ<Integer>(MAX_N);
+    assertEquals(0, pq.size());
+    for (int i = 0; i < MAX_N; i++) {
+      pq.insert(i);
+      assertEquals(i + 1, pq.size());
+    }
+  }
+
+  @Test
+  public void testInsertAndDelMax() {
+    MaxPQ pq = new MaxPQ<Integer>(MAX_N);
+    pq.insert(20);
+    assertEquals(20, pq.delMax());
+    pq.insert(100);
+    pq.insert(20);
+    pq.insert(40);
+    assertEquals(100, pq.delMax());
+    pq.insert(200);
+    assertEquals(200, pq.delMax());
+    assertEquals(40, pq.delMax());
+    assertEquals(20, pq.delMax());
+  }
 }
